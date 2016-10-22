7989b2a1db12b34ea91d711c920ec204e422009e
EsDeveloping <esfordeveloping@gmail.com>
2016-10-23 00:31:42 +0200

    Création de la classe principal et de la frame. Mais ces deux classe ne sont pas encor lié.

---
+++ Frame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Frame{
+	public Frame(String message){
+		JFrame frame = new JFrame();
+		JLabel label = new JLabel(message);
+		
+		frame.getContentPane().add(label);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
